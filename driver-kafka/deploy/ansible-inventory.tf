19d2aeaba59c24ebc4835d93fdae4d44b8473486
Andy Malakov <amalakov@deltixlab.com>
2020-07-13 18:29:36 -0400

    switching to private IPs for internal connections

--- driver-kafka/deploy/ansible-inventory.tf
+++ driver-kafka/deploy/ansible-inventory.tf
@@ -2,9 +2,9 @@
 resource "local_file" "hosts_cfg" {
   content = templatefile("${path.module}/templates/ansible-hosts.tpl",
   {
-    zookeeper_hosts = aws_instance.zookeeper.*.public_ip
-    kafka_hosts = aws_instance.kafka.*.public_ip
-    client_hosts = aws_instance.client.*.public_ip
+    zookeeper_hosts = aws_instance.zookeeper.*.private_ip
+    kafka_hosts = aws_instance.kafka.*.private_ip
+    client_hosts = aws_instance.client.*.private_ip
   }
   )
   filename = "./ansible-hosts.ini"
