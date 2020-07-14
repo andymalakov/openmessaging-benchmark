2178cb4f5e39b754c0e8c589cfefb60ae4870c25
Andy Malakov <amalakov@deltixlab.com>
2020-07-13 23:42:34 -0400

    debugging..

--- driver-kafka/deploy/ansible-inventory.tf
+++ driver-kafka/deploy/ansible-inventory.tf
@@ -2,9 +2,9 @@
 resource "local_file" "hosts_cfg" {
   content = templatefile("${path.module}/templates/ansible-hosts.tpl",
   {
-    zookeeper_hosts = aws_instance.zookeeper.*.public_ip
-    kafka_hosts = aws_instance.kafka.*.public_ip
-    client_hosts = aws_instance.client.*.public_ip
+    zookeeper_hosts = aws_instance.zookeeper.*
+    kafka_hosts = aws_instance.kafka.*
+    client_hosts = aws_instance.client.*
   }
   )
   filename = "./ansible-hosts.ini"
--- driver-kafka/deploy/templates/ansible-hosts.tpl
+++ driver-kafka/deploy/templates/ansible-hosts.tpl
@@ -1,14 +1,14 @@
 [zookeeper]
 %{ for host in zookeeper_hosts ~}
-${host}
+${host.public_ip}
 %{ endfor ~}
 
 [kafka]
 %{ for host in kafka_hosts ~}
-${host}
+${host.public_ip}
 %{ endfor ~}
 
 [client]
 %{ for host in client_hosts ~}
-${host}
+${host.public_ip}
 %{ endfor ~}
